c89de60b406a8707739ce8b032e7c36c95505341
asjadanis <asjadanis128@gmail.com>
2022-12-30 18:23:24 -0600

    chore: initial setup

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Roma",
+      animal: "Dog",
+      breed: "German-Shepherd",
+    }),
+    React.createElement(Pet, {
+      name: "Chotti",
+      animal: "Cat",
+      breed: "Persian",
+    }),
+    React.createElement(Pet, {
+      name: "Bella",
+      animal: "Cat",
+      breed: "Persian",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
