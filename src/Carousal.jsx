f2416e71d046640a281b3d00b2069515758a5a38
asjadanis <asjadanis128@gmail.com>
2023-01-01 12:18:37 -0600

    feat: add carousal component

---
+++ src/Carousal.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+
+class Carousal extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal hero" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => {
+            return (
+              // eslint-disable-next-line
+              <img
+                onClick={this.handleClick}
+                key={photo}
+                src={photo}
+                data-index={index}
+                className={index === active ? "active" : ""}
+                alt="animal thumbnail"
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousal;
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
+import Carousal from "./Carousal";
 
 const Details = () => {
   const { id } = useParams();
@@ -18,6 +19,7 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousal images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>
