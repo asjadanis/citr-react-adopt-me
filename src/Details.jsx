811f60f37d0c3499e5b82abd862ebd1c44a880f2
asjadanis <asjadanis128@gmail.com>
2023-01-01 00:01:06 -0600

    feat: enable react-query and react-router

---
+++ src/Details.jsx
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+
+const Details = () => {
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">߷</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.state} - {pet.city}
+        </h2>
+        <button> Adobt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
+
+  return <h2>{id}</h2>;
+};
+
+export default Details;
